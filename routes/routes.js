9f467bada5f9e30ff93e8c1d954c7bae8f98ae88
HumbleWebDev <tom@humblewebdev.com>
2019-03-27 18:08:25 -0500

    added route to change homeRooms table

--- routes/routes.js
+++ routes/routes.js
@@ -2,26 +2,36 @@ var db = require("../models");
 var Router = require('express').Router();
 
 Router.get('/', function(request, response) {
-    db.Homes.create(
-        {
-            "title": "test"
-        }
-    ).then((home) => {
-        db.Rooms.create({
-            "title": "living room"
-        }).then((room) => {
-            home.addRoom(room, {through: {status: true}})
-            .then(() => {
-                db.Homes.findAll({include: [db.Rooms]}).then(homes => {
-                    return response.json(homes);
+        db.Homes.create(
+            {
+                "title": "test"
+            }
+        ).then((home) => {
+            db.Rooms.create({
+                "title": "living room"
+            }).then((room) => {
+                home.addRoom(room, {through: {status: true}})
+                .then(() => {
+                    db.Homes.findAll({include: [db.Rooms]}).then(homes => {
+                        return response.json({home: homes});
                 }) ;
             });
         });
     });
+});
 
-    
-
-    
+Router.get('/home/:homeId/room/:roomId', (request, response) => {
+    db.HomeRooms.findOne({
+        where: {
+            homeId: request.params.homeId,
+            roomId: request.params.roomId
+        }
+    }).then((homeRoom) => {
+        homeRoom.status = false;
+        homeRoom.save().then(() => {
+            return response.json({homeRoom: homeRoom});
+        }).catch(console.log());
+    }).catch(console.log);
 });
 
 module.exports = Router;
